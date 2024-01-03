4d1f73bf38ce50123e50df910b2926071012d837
Gabriel Miller <102246954+Mr-GEM-Bones@users.noreply.github.com>
2024-01-02 19:06:06 -0500

    Overworld Updates

--- Scenes/overworld/overworld.gd
+++ Scenes/overworld/overworld.gd
@@ -1,6 +1,10 @@
 extends Node
 
 var walking = true
+var combat = false
+@onready var timer = $EnemyTimer
+@onready var enemy_anim = $EnemyPath/EnemyAnimation
+@onready var potion_anim = $Wizard/PotionAnimation
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,5 +12,47 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	#toggle combat
+	
+	
+	#make background move while walking
 	if walking == true:
 		$ParallaxBackground.scroll_offset.x -= 200 * delta
+
+func _input(_ev):
+	#start and stop walking
+
+	#if Input.is_key_pressed(KEY_W):
+	#	if walking == true:
+	#		walking = false
+	#	elif walking == false and anim.is_playing() == false:
+	#		walking = true
+
+	#throw poiton
+	if Input.is_key_pressed(KEY_T) and combat == true and potion_anim.is_playing() == false:
+		$Wizard.throw_potion()
+
+
+func end_combat():
+	timer.wait_time = 6
+	combat = false
+	enemy_anim.play("RESET")
+	walking = true
+	timer.paused = false
+	print(timer.wait_time)
+	
+
+func _on_enemy_timer_timeout():
+	enemy_anim.play("enemy_animation")
+	timer.paused = true
+	
+func _on_enemy_animation_animation_finished(anim_name):
+	if anim_name == "enemy_animation":
+		combat = true
+		walking = false
+		print(anim_name)
+	
+
+func _on_wizard_sig_end_combat():
+	end_combat()
--- entities/wizard/wizard.gd
+++ entities/wizard/wizard.gd
@@ -1,11 +1,25 @@
 extends Node2D
 
-
+@onready var potion_sprite = $Path2D/PathFollow2D/ThrownPotion
+@onready var anim = $PotionAnimation
+signal sig_end_combat
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+
+
+func throw_potion():
+	potion_sprite.visible = true
+	anim.play("potion_throw")
+	#if anim.animation_finished("potion_throw"):
+		#pass
+
+
+func _on_potion_animation_animation_finished(_anim_name):
+	potion_sprite.visible = false
+	emit_signal("sig_end_combat")
