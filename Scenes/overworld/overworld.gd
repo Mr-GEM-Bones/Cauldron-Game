b7f5bf4b69f42ebd29cbc26f6da3f6312c1a4acd
Gabriel Miller <102246954+Mr-GEM-Bones@users.noreply.github.com>
2024-01-04 13:07:37 -0500

    Updated functions

--- Scenes/overworld/overworld.gd
+++ Scenes/overworld/overworld.gd
@@ -12,17 +12,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	#toggle combat
-	
-	
 	#make background move while walking
 	if walking == true:
 		$ParallaxBackground.scroll_offset.x -= 200 * delta
 
 func _input(_ev):
-	#start and stop walking
-
 	#if Input.is_key_pressed(KEY_W):
 	#	if walking == true:
 	#		walking = false
@@ -30,29 +24,44 @@ func _input(_ev):
 	#		walking = true
 
 	#throw poiton
-	if Input.is_key_pressed(KEY_T) and combat == true and potion_anim.is_playing() == false:
-		$Wizard.throw_potion()
+	#if Input.is_key_pressed(KEY_T) and combat == true and potion_anim.is_playing() == false:
+	#	$Wizard.throw_potion()
+	pass
 
+# Wizard throws the potion
+func potion():
+	$Wizard.throw_potion()
 
-func end_combat():
-	timer.wait_time = 6
-	combat = false
-	enemy_anim.play("RESET")
-	walking = true
-	timer.paused = false
-	print(timer.wait_time)
-	
-
-func _on_enemy_timer_timeout():
+# Call to start combat
+func start_combat():
+	print("Combat started")
 	enemy_anim.play("enemy_animation")
 	timer.paused = true
-	
+	print("Timer paused")
+
+# Calls when enemy is done with intro animation
 func _on_enemy_animation_animation_finished(anim_name):
 	if anim_name == "enemy_animation":
 		combat = true
 		walking = false
 		print(anim_name)
-	
 
+# Call to end combat
+func end_combat():
+	print("Combat ended")
+	timer.wait_time = 6
+	combat = false
+	enemy_anim.play("RESET")
+	walking = true
+	timer.paused = false
+	print("Timer reset")
+
+# Start combat when timer ends
+func _on_enemy_timer_timeout():
+	start_combat()
+
+# End combat when wizard sends signal
 func _on_wizard_sig_end_combat():
 	end_combat()
+
+#GEM 01/04/2024
--- entities/wizard/wizard.gd
+++ entities/wizard/wizard.gd
@@ -7,19 +7,18 @@ signal sig_end_combat
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
-
+#throws potion
 func throw_potion():
 	potion_sprite.visible = true
 	anim.play("potion_throw")
 	#if anim.animation_finished("potion_throw"):
 		#pass
 
-
+#ends combat when potion hits
 func _on_potion_animation_animation_finished(_anim_name):
 	potion_sprite.visible = false
 	emit_signal("sig_end_combat")
